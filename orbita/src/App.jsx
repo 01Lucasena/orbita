624b56f16558eb4b5178bdc762722f40bec07984
Lucas <lucasena020@gmail.com>
2026-08-14 00:45:27 -0300

    feat: adiciona rotas /perfil/:uid e /editar-perfil

--- orbita/src/App.jsx
+++ orbita/src/App.jsx
@@ -2,7 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { AuthProvider } from "./context/AuthContext"
 import Login from './pages/login/Login'
 import Feed from "./pages/feed/Feed"
+import Cadastro from "./pages/cadastro/Cadastro"
+import RecuperarSenha from "./pages/recuperarSenha/RecuperarSenha"
 import ProtectedRoute from "./components/ProtectedRoute"
+import Perfil from "./pages/perfil/Perfil"
+import EditarPerfil from "./pages/editarPerfil/EditarPerfil"
 import './App.css'
 
 function App() {
@@ -19,6 +23,18 @@ function App() {
                     <Feed />
                 </ProtectedRoute>
                 }/>
+                <Route path="/cadastro" element={<Cadastro/>}/>
+                <Route path="/recuperar-senha" element={<RecuperarSenha />} />
+                <Route path="/perfil/:uid" element={
+                <ProtectedRoute>
+                    <Perfil />
+                </ProtectedRoute>
+                } />
+                <Route path="/editar-perfil" element={
+                <ProtectedRoute>
+                    <EditarPerfil />
+                </ProtectedRoute>
+                } />
             </Routes>
         </BrowserRouter>
      </AuthProvider>
